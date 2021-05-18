f777400638d7e13e2ec006fbaa00add00ec38891
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-05-18 22:46:37 +0530

    Completed UI elements for the Match Page

---
+++ ipl-dashboard/src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,13 @@
+import { React, useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom';
+import { MatchSmallCard } from '../components/MatchSmallCard';
+import { MatchDetailCard } from '../components/MatchDetailCard'
+
+export const MatchPage = () => {
+    
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
