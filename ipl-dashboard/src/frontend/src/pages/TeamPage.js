0a64cd9e270340a7982c22cf7d8505faccacf8b2
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-05-18 21:21:52 +0530

    Rendering the real data in the react components

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,15 +1,28 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { MatchDetailCard } from '../components/MatchDetailCard'
 
 export const TeamPage = () => {
+    const [team, setTeam] = useState({matches: []});
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch('http://localhost:8080/teams/Rajasthan Royals');
+                const data = await response.json();
+                setTeam(data);
+            };
+            fetchMatches();
+        }, []
+    )
+
   return (
     <div className="TeamPage">
-      <h1>IPL Dashboard</h1>
-      <MatchDetailCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
+      <h1>{team.teamName}</h1>
+      
+      <MatchDetailCard match={team.matches[0]} />
+      {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
+
     </div>
   );
 }
